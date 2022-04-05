e1789060a33ea97aaffe72a6b7d4069127ad4bfc
Rafia Sultana <rafiasultana0097@gmail.com>
2022-04-05 21:03:37 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <nav>
-
-                <h1>Header</h1>
-
-
-
-            </nav>
+        <div className='header-container'>
+            <Link to='/' className='book-container' >Book Gallery</Link>
+            <div>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/reviews'>Review</NavLink>
+                <NavLink to='/dashboard' >Dashboard</NavLink>
+                <NavLink to='/blogs'>Blogs</NavLink>
+                <NavLink to='/about'>About</NavLink>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import UseReview from '../Hook/UseReview';
+import './Home.css';
+
 
 const Home = () => {
+    const [reviews, setReviews] = UseReview();
     return (
         <div>
-            <h1>home</h1>
+            <div className='container'>
+
+                <div className="paragraph">
+                    <h1>Thy yourself!</h1>
+                    <p>Books are the power of knowledge.By reading this book,the reader can gain real life knowledge and relate themselves with world.</p>
+                </div>
+                <div className="image" >
+                    <img src={'https://previews.123rf.com/images/cre8tive/cre8tive0609/cre8tive060900117/520159-libri-3d-impilati-in-cima-a-eachother.jpg'} alt="" />
+                </div>
+            </div>
+            <hr></hr>
+            <div className="">
+                <h2 className='review-title'>Customer Reviews</h2>
+                <div className="">
+                    {
+
+                    }
+                </div>
+            </div>
+
+
         </div>
     );
 };
