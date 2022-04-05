293cb657c26577c0202e40acc00b10c05473ffd2
Rafia Sultana <rafiasultana0097@gmail.com>
2022-04-05 19:49:28 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+
+                <h1>Header</h1>
+
+
+
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
